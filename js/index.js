d578a28d4c9a663281daaa6e3d3924d8e17f97e8
zsn <z1108n@163.com>
2019-01-09 16:06:43 +0800

    京东首页

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// tag切换
+$('.main_list ul li a').hover(function () {
+    $('.none').show();
+    $('.main_r').hide();
+}, function () {
+    $('.none').hide();
+    $('.main_r').show();
+});
+
+// 秒杀轮播
